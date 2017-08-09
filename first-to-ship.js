d07bf84ea052fdb7037510fe94f1fbcaa04a9b56
drufball <knox.dru@gmail.com>
2017-08-09 17:21:19 -0400

    add scripts to sanitize data and analyze first to ship

--- first-to-ship.js
+++ first-to-ship.js
@@ -1,10 +1,78 @@
 const fs = require('fs')
 const readline = require('readline')
-
 const rl = readline.createInterface({
-  input: fs.createReadStream('apiData.csv')
+  input: fs.createReadStream('sanitized-api-data.txt')
 })
 
+let firstToShipCounts = {
+  "Chrome": 0,
+  "Firefox": 0,
+  "Edge": 0,
+  "Safari": 0
+}
+let shippedFirstWithFollowers = {
+  "Chrome": 0,
+  "Firefox": 0,
+  "Edge": 0,
+  "Safari": 0
+}
+let ambiguousEdgeOrSafariCount = {
+  "Chrome": 0,
+  "Firefox": 0
+}
 rl.on('line', (line) => {
-  console.log(`Line from file: ${line}`)
+  const fields = line.split(",")
+  const interface = fields[0]
+  const api = fields[1]
+
+  if( fields.length > 2 ) {
+    let firstReleaseData = fields[2].split(" ")
+    let browser = firstReleaseData[0]
+    let version = firstReleaseData[1]
+    if(releaseTooEarly(browser, version)) {
+      return
+    }
+
+    firstToShipCounts[browser]++
+    if( fields.length > 3 ) {
+      if( browser == "Chrome" || browser == "Firefox" ) {
+        for( let i = 3; i < fields.length; i++ ) {
+          let followerData = fields[i].split(" ")
+          let followerBrowser = followerData[0]
+          let followerVersion = followerData[1]
+
+          if( releaseTooEarly(followerBrowser, followerVersion) ) {
+            ambiguousEdgeOrSafariCount[browser]++
+          }
+        }
+      }
+
+      shippedFirstWithFollowers[browser]++
+    }
+  }
 })
+
+function releaseTooEarly(browser, version) {
+  let firstReleases = {
+    "Chrome":["38"],
+    "Firefox":["34"],
+    "Edge":["13"],
+    "Safari":["601"]
+  }
+
+  return firstReleases[browser].includes(version)
+}
+
+rl.on('close', (line) => {
+  console.log(firstToShipCounts)
+  console.log(shippedFirstWithFollowers)
+  console.log(ambiguousEdgeOrSafariCount)
+
+  for( browser of Object.keys(firstToShipCounts) ) {
+    console.log(positiveElasticStretch(browser))
+  }
+})
+
+function positiveElasticStretch(browser) {
+  return `${browser}: ${shippedFirstWithFollowers[browser]/firstToShipCounts[browser]}`
+}
---
+++ sanitize-api-data.js
@@ -0,0 +1,96 @@
+const fs = require('fs')
+const readline = require('readline')
+const rl = readline.createInterface({
+  input: fs.createReadStream('apiData.csv')
+})
+
+let headingsToCheck = {}
+let implementationStatusForFeature = []
+rl.on('line', (line) => {
+  cols = Object.keys(headingsToCheck)
+  if(cols.length == 0) {
+    headingsToCheck = parseHeadings(line)
+    return
+  }
+
+  let implementers = []
+  let implementedForRelease = line.split(",")
+  let interf = implementedForRelease[0]
+  let api = implementedForRelease[1]
+
+  for( let col of cols ) {
+    let i = parseInt(col)
+    let releaseSupport = implementedForRelease[i]
+
+    if( releaseSupport == "true" ) {
+      let releaseToAdd = headingsToCheck[col]
+      let shouldAdd = true
+      for(let implementer of implementers) {
+        if( releaseToAdd.browser == implementer.browser ) {
+          shouldAdd = false
+        }
+      }
+
+      if(shouldAdd) {
+        implementers.push(headingsToCheck[col])
+      }
+    }
+  }
+  implementationStatusForFeature.push({interf, api, implementers})
+})
+
+rl.on('close', () => {
+  fs.writeFile('sanitized-api-data.txt', convertToString(implementationStatusForFeature), (err) => {
+    if(err) {
+      console.log(err)
+    }
+    else {
+      console.log("Sanitized API data saved successfully.")
+    }
+  })
+})
+
+function convertToString(apisWIthReleaseHistory) {
+  let str = ""
+  for( entry of apisWIthReleaseHistory ) {
+    str += `${entry.interf},${entry.api}`
+    for( release of entry.implementers ) {
+      str += `,${release.describe()}`
+    }
+    str += "\n"
+  }
+  return str
+}
+
+function parseHeadings(row) {
+  const fields = row.split(",")
+
+  let fieldToSupportedRelease = {}
+  for(var col = 2; col < fields.length; col++) {
+    let release = new BrowserRelease(fields[col])
+    if( !isSupportedBrowser(release) ) {
+      continue
+    }
+    fieldToSupportedRelease[col] = release
+  }
+  return fieldToSupportedRelease
+}
+
+class BrowserRelease {
+  constructor(releaseString) {
+    let metadata = releaseString.split("_")
+    this.browser = metadata[0]
+    let version = metadata[1]
+    this.platform = metadata[2]
+
+    this.majorVersion = version.split(".")[0]
+  }
+
+  describe() {
+    return `${this.browser} ${this.majorVersion}`
+  }
+}
+
+function isSupportedBrowser(release) {
+  return release.browser == "Safari" || release.platform == "Windows"
+}
