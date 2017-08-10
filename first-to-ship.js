03c6616f35ee90bc22a7be5a679905a0eecbb8bf
drufball <knox.dru@gmail.com>
2017-08-10 17:23:09 -0400

    updated data set with more safari info, added explainer to readme.

--- first-to-ship.js
+++ first-to-ship.js
@@ -1,7 +1,6 @@
 const fs = require('fs')
 const readline = require('readline')
-
-const releaseDates = JSON.parse(fs.readFileSync('release-dates.json', {'encoding':'utf-8'}))
+const helpers = require('./helpers.js')
 
 const rl = readline.createInterface({
   input: fs.createReadStream('sanitized-api-data.txt')
@@ -31,8 +30,11 @@ let dateDiffs = {
   "Edge": [],
   "Safari": []
 }
-rl.on('line', (line) => {
-  const fields = line.split(",")
+rl.on('line', computeImplementationOrder)
+rl.on('close', printResults)
+
+function computeImplementationOrder(implData) {
+  const fields = implData.split(",")
   const interface = fields[0]
   const api = fields[1]
 
@@ -40,7 +42,7 @@ rl.on('line', (line) => {
     let firstReleaseData = fields[2].split(" ")
     let browser = firstReleaseData[0]
     let version = firstReleaseData[1]
-    if(firstRelease(browser, version) && (browser == "Chrome" || browser == "Firefox")) {
+    if(helpers.firstRelease(browser, version) && (browser != "Edge")) {
       return
     }
 
@@ -51,7 +53,7 @@ rl.on('line', (line) => {
         let followerBrowser = followerData[0]
         let followerVersion = followerData[1]
 
-        if( firstRelease(followerBrowser, followerVersion) ) {
+        if( helpers.firstRelease(followerBrowser, followerVersion) ) {
           ambiguousCases[browser]++
           ambiguous = true
         }
@@ -63,93 +65,36 @@ rl.on('line', (line) => {
         let followerData = fields[3].split(" ")
         let followerBrowser = followerData[0]
         let followerVersion = followerData[1]
-        dateDiffs[browser].push(computeReleaseDateDifference(browser, version, followerBrowser, followerVersion))
+        let dateDiff = helpers.computeReleaseDateDifference(browser, version, followerBrowser, followerVersion)
+        dateDiffs[browser].push(dateDiff)
       }
     }
     else {
       firstToShipCounts[browser]++
     }
   }
-})
-
-function firstRelease(browser, version) {
-  let firstReleases = {
-    "Chrome":["38"],
-    "Firefox":["34"],
-    "Edge":["13"],
-    "Safari":["601"]
-  }
-
-  return firstReleases[browser].includes(version)
 }
 
-rl.on('close', (line) => {
-  console.log(firstToShipCounts)
-  console.log(shippedFirstWithFollowers)
-  console.log(ambiguousCases)
-
+function printResults() {
   for( browser of Object.keys(firstToShipCounts) ) {
-    console.log(minimumPositiveElasticStretch(browser))
-    console.log(maximumPositiveElasticStretch(browser))
-    console.log(`Avg delay: ${average(dateDiffs[browser])} Median delay: ${median(dateDiffs[browser])}`)
-  }
-  console.log(dateDiffs["Firefox"])
-})
+    let minStretch = helpers.convertToPercent(minimumPositiveElasticStretch(browser))
+    let maxStretch = helpers.convertToPercent(maximumPositiveElasticStretch(browser))
+    console.log(`${browser}:`)
+    console.log(`Shipped ${firstToShipCounts[browser]} to ${firstToShipCounts[browser] + ambiguousCases[browser]} APIs first`)
+    console.log(`${minStretch}-${maxStretch}% of APIs shipped first have another implementation`)
+    console.log(`Average time to second implementation: ${Math.round(helpers.average(dateDiffs[browser]))} days`)
+    console.log(`Median time to second implementation: ${Math.round(helpers.median(dateDiffs[browser]))} days`)
 
-function average(arr) {
-  let sum = 0
-  for(let el of arr) {
-    sum += el
+    console.log('')
   }
-  return sum / arr.length
-}
-
-function median(arr) {
-  let sorted = arr.sort((a, b) => a - b)
-  if( sorted.length % 2 == 0 ) {
-    let mid = sorted.length / 2
-    return (sorted[mid] + sorted[mid+1]) / 2
-  }
-  return sorted[Math.round(sorted.length/2)]
 }
 
 function minimumPositiveElasticStretch(browser) {
-  return `${browser}: ${shippedFirstWithFollowers[browser]/firstToShipCounts[browser]}`
+  return shippedFirstWithFollowers[browser] / firstToShipCounts[browser]
 }
 
 function maximumPositiveElasticStretch(browser) {
   let maxShippedWithFollowers = shippedFirstWithFollowers[browser] + ambiguousCases[browser]
   let maxFirstToShip = firstToShipCounts[browser] + ambiguousCases[browser]
-  return `${browser}: ${maxShippedWithFollowers/maxFirstToShip}`
-}
-
-function computeReleaseDateDifference(browser1, version1, browser2, version2) {
-  release1 = releaseDates[browser1][version1].split("-")
-  release2 = releaseDates[browser2][version2].split("-")
-
-  let dateDifference = computeDateDifference(release1, release2)
-
-  return dateDifference
-}
-
-function computeDateDifference(date1, date2) {
-  let dateDifference = ( parseInt(date2[0]) - parseInt(date1[0]) ) * 365
-  let month1 = parseInt(date1[1])
-  let month2 = parseInt(date2[1])
-  if(month1 <= month2) {
-    dateDifference += ( month2 - month1 ) * 30
-  }
-  else {
-    dateDifference += (12 - month1 + month2) * 30
-  }
-  let day1 = parseInt(date1[2])
-  let day2 = parseInt(date2[2])
-  if(day1 <= day2) {
-    dateDifference += day2 - day1
-  }
-  else {
-    dateDifference += 30 - day1 + day2
-  }
-
-  return dateDifference
+  return maxShippedWithFollowers / maxFirstToShip
 }
---
+++ helpers.js
@@ -0,0 +1,67 @@
+const fs = require('fs')
+const releaseDates = JSON.parse(fs.readFileSync('release-dates.json', {'encoding':'utf-8'}))
+
+module.exports = {
+  firstRelease: function(browser, version) {
+    let firstReleases = {
+      "Chrome":["38"],
+      "Firefox":["34"],
+      "Edge":["13"],
+      "Safari":["534"]
+    }
+
+    return firstReleases[browser].includes(version)
+  },
+
+  computeReleaseDateDifference: function(browser1, version1, browser2, version2) {
+    release1 = releaseDates[browser1][version1].split("-")
+    release2 = releaseDates[browser2][version2].split("-")
+
+    let dateDifference = computeDateDifference(release1, release2)
+
+    return dateDifference
+  },
+
+  average: function(arr) {
+    let sum = 0
+    for(let el of arr) {
+      sum += el
+    }
+    return sum / arr.length
+  },
+
+  median: function(arr) {
+    let sorted = arr.sort((a, b) => a - b)
+    if( sorted.length % 2 == 0 ) {
+      let mid = sorted.length / 2
+      return (sorted[mid] + sorted[mid+1]) / 2
+    }
+    return sorted[Math.round(sorted.length/2)]
+  },
+
+  convertToPercent: function(decimal) {
+    return Math.round(decimal * 100)
+  }
+}
+
+function computeDateDifference(date1, date2) {
+  let dateDifference = ( parseInt(date2[0]) - parseInt(date1[0]) ) * 365
+  let month1 = parseInt(date1[1])
+  let month2 = parseInt(date2[1])
+  if(month1 <= month2) {
+    dateDifference += ( month2 - month1 ) * 30
+  }
+  else {
+    dateDifference += (12 - month1 + month2) * 30
+  }
+  let day1 = parseInt(date1[2])
+  let day2 = parseInt(date2[2])
+  if(day1 <= day2) {
+    dateDifference += day2 - day1
+  }
+  else {
+    dateDifference += 30 - day1 + day2
+  }
+
+  return dateDifference
+}
